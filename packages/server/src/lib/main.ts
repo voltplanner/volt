e0411dc65aeff00cfb94d6659788855ae02cd6c5
temarusanov <artem.rusanov.02@gmail.com>
2024-01-27 17:44:35 +0700

    chore: configure project server

--- packages/server/src/lib/main.ts
+++ packages/server/src/lib/main.ts
@@ -1,20 +1,27 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { Logger } from '@tinybudgie/logger'
+import { get } from 'env-var'
 
+import { environment } from '../environments/environment'
 import { AppModule } from './app.module'
 
+const logger = new Logger()
+
+logger.configure({
+    json: environment.production
+})
+
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create(AppModule, { logger })
+
     const globalPrefix = 'api'
+
     app.setGlobalPrefix(globalPrefix)
-    const port = process.env.PORT || 3000
+
+    const port = get('SERVER_PORT').default(3000).asPortNumber()
     await app.listen(port)
-    Logger.log(
+
+    logger.log(
         `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
     )
 }
