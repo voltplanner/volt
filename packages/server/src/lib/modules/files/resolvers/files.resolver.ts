2fb81f4ffd4feeadba2a99ea8117e3d3214cd205
den <adalll@mail.ru>
2024-02-14 07:47:47 +0800

    feat(backend): graphql upload implemented

--- packages/server/src/lib/modules/files/resolvers/files.resolver.ts
+++ packages/server/src/lib/modules/files/resolvers/files.resolver.ts
@@ -2,7 +2,9 @@ import { UseGuards } from '@nestjs/common'
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 
 import { AccessControl } from '../../../shared/decorators'
+import { CurrentUser } from '../../../shared/decorators/current-user.decorator'
 import { ACLGuard } from '../../../shared/guards/acl.guard'
+import { CurrentUserPayload } from '../../../shared/interfaces/shared.interfaces'
 import { GetFileInput, UploadFileInput } from '../interfaces/files.graphql'
 import { FilesService } from '../services/files.service'
 
@@ -18,9 +20,15 @@ export class FilesResolver {
     }
 
     @UseGuards(ACLGuard)
-    @Mutation(() => Boolean)
+    @Mutation(() => String)
     @AccessControl({ group: 'files', description: 'Upload file' })
-    async uploadFile(@Args('input') input: UploadFileInput): Promise<boolean> {
-        return await this.filesService.uploadFile(input)
+    async uploadFile(
+        @Args('input') input: UploadFileInput,
+        @CurrentUser() user: CurrentUserPayload,
+    ): Promise<string> {
+        return await this.filesService.uploadFileGraphql(
+            input.file,
+            user.userId,
+        )
     }
 }
