e3e7e37e590728dd3bbba7cc2e6e129d091f64bb
Ilusaaxd <jlimonka@mail.ru>
2024-03-03 22:23:09 +0700

    feat: added navigation back for edit role

--- packages/client/src/lib/pages/EditRolePage/ui/EditRolePage.tsx
+++ packages/client/src/lib/pages/EditRolePage/ui/EditRolePage.tsx
@@ -1,13 +1,25 @@
 import { EditRole } from 'features'
-import { useParams } from 'react-router-dom'
-import { Header } from 'shared'
-import { PageContainerStyled, WrapperStyled, ContentStyled } from './styles'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Button, Header } from 'shared'
+import {
+    PageContainerStyled,
+    WrapperStyled,
+    ContentStyled,
+    ButtonsContainer,
+} from './styles'
 
 const EditRolePage = () => {
     const { id } = useParams()
+    const navigate = useNavigate()
+    const handleNavigate = () => {
+        navigate('/roles')
+    }
     return (
         <PageContainerStyled>
             <WrapperStyled>
+                <ButtonsContainer>
+                    <Button onClick={handleNavigate}>{'< Back'}</Button>
+                </ButtonsContainer>
                 <ContentStyled>
                     <Header title={'Owner permissions'} />
                     <EditRole name={id ?? ''} />
