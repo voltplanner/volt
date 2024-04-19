b818e56ce60a7ce1559177afb8ac9f461dd29755
temz <artem.rusanov.02@gmail.com>
2024-04-19 23:03:33 +0700

    chore: format code

--- packages/client/src/lib/features/RoleList/api/useRoles.ts
+++ packages/client/src/lib/features/RoleList/api/useRoles.ts
@@ -21,8 +21,8 @@ export const useRoles = () => {
                     group: true,
                     id: true,
                     name: true,
-                }
-            }
+                },
+            },
         },
     })
 
